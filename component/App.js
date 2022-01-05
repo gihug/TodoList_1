44103afc335ec8b65fed870aac3d1a646a7428f2
unknown <l2rabcxyz@gmail.com>
2022-01-05 22:59:36 +0700

    event click todo item

--- component/App.js
+++ component/App.js
@@ -2,11 +2,15 @@ import html from "../core.js";
 import Footer from "./Footer.js";
 import Header from "./Header.js";
 import TodoList from "./TodoList.js";
+import { connect } from "../store.js";
 
-function App() {
+function App({ todos }) {
   return html`
-    <section class="todoapp">${Header()}${TodoList()}${Footer()}</section>
+    <section class="todoapp">
+      ${Header()} ${todos.length > 0 ? TodoList() : null}
+      ${todos.length > 0 ? Footer() : null}
+    </section>
   `;
 }
 
-export default App;
+export default connect()(App);
--- reducer.js
+++ reducer.js
@@ -1,26 +1,27 @@
+import storage from "./util/storage.js";
+
 const init = {
-  todos: [
-    {
-      title: "Learn Javascript",
-      completed: false,
-    },
-    {
-      title: "Learn HTML, CSS",
-      completed: true,
-    },
-  ],
+  todos: storage.get(),
+};
+
+// gộp tất cả các action
+const actions = {
+  // action thêm mới vào list, nhận state ban đầu và title thêm mới
+  add({ todos }, title) {
+    if (title) {
+      todos.push({ title, completed: false });
+      storage.set(todos);
+    }
+  },
+  toggle({ todos }, index) {
+    const todo = todos[index];
+    todo.completed = !todo.completed;
+    storage.set(todos);
+  },
 };
 
 export default function reducer(state = init, action, args) {
-  switch (action) {
-    case "add": {
-      const [title] = args;
-      console.log({
-        ...state,
-      });
-      return { ...state, todos: [...state.todos, { title, completed: false }] };
-    }
-    default:
-      return state;
-  }
+  // Nếu tồn tại action với key thì sẽ chạy hàm với key action cùng tham số
+  actions[action] && actions[action](state, ...args);
+  return state;
 }
